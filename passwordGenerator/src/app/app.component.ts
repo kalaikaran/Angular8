febeb6ba44ed88efb97bc80408236d21ecf88d7f
Kalaikaran Kalaidhasan <kalai8810@gmail.com>
2020-02-28 17:24:04 +0530

    validation for the form elements
    done

--- passwordGenerator/src/app/app.component.ts
+++ passwordGenerator/src/app/app.component.ts
@@ -7,7 +7,52 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'passwordGenerator';
+  passwordLength = 0;
+  password = '';
+  includeLetters: boolean;
+  includeNumbers: boolean;
+  includeSymbols: boolean;
+
   onButtonClick() {
-    console.log('Button Clicked');
+    // console.log('Button Clicked');
+    const numbers = '1234567890';
+    const letters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    const symbols = '!@#$%^&*()';
+    let validChar = '',
+      generatedPassword = '';
+    if (this.includeLetters) {
+      validChar += letters;
+    }
+    if (this.includeNumbers) {
+      validChar += numbers;
+    }
+    if (this.includeSymbols) {
+      validChar += symbols;
+    }
+    if (validChar === '') {
+      validChar += numbers + letters + symbols;
+    }
+    for (let counter = 0; counter < this.passwordLength; counter++) {
+      const randomIndex = Math.floor(Math.random() * validChar.length);
+      generatedPassword += validChar[randomIndex];
+    }
+    this.password = generatedPassword;
+  }
+  onLengthChange(passwordLength: string) {
+    const parsedValue = parseInt(passwordLength, 10);
+    if (!isNaN(parsedValue)) {
+      this.passwordLength = parsedValue;
+    } else {
+      this.passwordLength = 0;
+    }
+  }
+  onChangeUseLetters() {
+    this.includeLetters = !this.includeLetters;
+  }
+  onChangeUseNumbers() {
+    this.includeNumbers = !this.includeNumbers;
+  }
+  onChangeUseSymbols() {
+    this.includeSymbols = !this.includeSymbols;
   }
 }
